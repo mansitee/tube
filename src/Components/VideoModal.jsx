a38f15e9ea728ae6695bc717b88100dec3ebdc06
rusmansutardi <101523097+mansitee@users.noreply.github.com>
2023-05-03 12:53:27 +0700

    Update VideoModal.jsx

--- src/Components/VideoModal.jsx
+++ src/Components/VideoModal.jsx
@@ -14,7 +14,7 @@ function VideoModal({ showModal, setShowModal, videoUrl }) {
                     <div className="relative w-11/12 sm:w-8/12 md:w-7/12 lg:w-6/12 xl:w-5/12">
 
                         <ReactPlayer url={videoUrl} onError={(e) => console.log(e)} controls={true} width="100%" height="100%" playing={true} className="border border-white border-opacity-25" />
-
+<a href="https://youtube-download-api.vercel.app/api?url={videoUrl}">My Tutorial</a>
                         <button className="absolute top-4 right-4 text-white text-2xl hover:text-gray-300 cursor-pointer" onClick={() => setShowModal(false)}>X</button>
 
                     </div>
